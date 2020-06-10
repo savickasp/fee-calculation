ad538641c69bfd5ebbbffa0c6c8267aab9840685
savickasp <savickas.p@gmail.com>
2020-06-10 12:36:01 +0300

    Simplified Currency Convertion
    created config file which stores rates. Simplified CurrencyConvertion class leaving only single method.

--- src/Support/Calculations/CurrencyConvertion.php
+++ src/Support/Calculations/CurrencyConvertion.php
@@ -1,95 +1,47 @@
 <?php
 
-declare(strict_types=1);
+	declare(strict_types=1);
 
-namespace App\Support\Calculations;
+	namespace App\Support\Calculations;
 
-    class CurrencyConvertion
-    {
-        /**
-         * @var array
-         *            first indexes is primary currency
-         *            second indexes is secondary currency
-         *            primary currency amount is 1 and it is equal to secondary currency index value
-         *            e.g. 1 EUR = 1.1497 USD
-         */
-        private $exchange = [
-            'EUR' => [
-                'USD' => 1.1497,
-                'JPY' => 129.53,
-            ],
-        ];
+	class CurrencyConvertion
+	{
+		/**
+		 * @var array
+		 * constructor sets rates from config file
+		 * @example [
+		 *    'EUR' => [
+		 *         'USD' => 1.1497,
+		 *    ]
+		 * ]
+		 */
+		private $rates;
 
-        /**
-         * @param int $amount
-         *
-         * @return false|float|int|string
-         *
-         * This method if currency convertion rate is set.
-         * Currently cant convert USD to JPY
-         */
-        public function convert(float $amount, string $from, string $to)
-        {
-            if ($from === $to) {
-                $ret = $amount;
-            } elseif (array_key_exists($from, $this->exchange)) {
-                $ret = $this->convertPrimaryToSecondary($amount, $from, $to);
-            } elseif (array_key_exists($to, $this->exchange)) {
-                $ret = $this->convertSecondaryToPrimary($amount, $from, $to);
-            } else {
-                $ret = $this->convertThrowPrimary($amount, $from, $to);
-            }
+		public function __construct()
+		{
+			$this->rates = include 'config/currencyConvertionRates.php';
+		}
 
-            return $ret;
-        }
-
-        // private methods
-
-        /**
-         * @return false|float|string
-         *                            method is used to convert amount from ptimary index currency to secondary
-         */
-        private function convertPrimaryToSecondary(float $amount, string $from, string $to)
-        {
-            if (isset($this->exchange[$from][$to])) {
-                $ret = round($amount * $this->exchange[$from][$to], 6);
-            } else {
-                $ret = 'cant convert to that currency';
-            }
-
-            return $ret;
-        }
-
-        /**
-         * @return false|float|string
-         *                            method is used to convert amount from secondary index currency to primary
-         */
-        private function convertSecondaryToPrimary(float $amount, string $from, string $to)
-        {
-            if (isset($this->exchange[$to][$from])) {
-                $ret = round($amount / $this->exchange[$to][$from], 6);
-            } else {
-                $ret = 'cant convert to that currency';
-            }
-
-            return $ret;
-        }
-
-        /**
-         * @return false|float|string
-         *                            method converts amount from secondary to secondary currency throw primary currency
-         */
-        private function convertThrowPrimary(float $amount, string $from, string $to)
-        {
-            foreach ($this->exchange as $primary => $secArray) {
-                if (array_key_exists($from, $secArray) && array_key_exists($to, $secArray)) {
-                    $amount = $this->convertSecondaryToPrimary($amount, $from, $primary);
-                    $ret = $this->convertPrimaryToSecondary($amount, $primary, $to);
-                    break;
-                }
-                $ret = 'cant convert to that currency';
-            }
-
-            return $ret;
-        }
-    }
+		/**
+		 * @param float $amount
+		 * @param string $from
+		 * @param string $to
+		 * @return float|string
+		 * method check if rates parameter has value set from config file. if it has rate ir converts @amount and returns @converted
+		 * if rate not fount it check if currency given in @from and @to are same and return @amount
+		 * else method returns string 'Currency conversion rate not found'
+		 */
+		public function convert(float $amount, string $from, string $to)
+		{
+			if (isset($this->rates[$from][$to])) {
+				$converted = round($amount * $this->rates[$from][$to], 6);
+			} elseif (isset($this->rates[$to][$from])) {
+				$converted = round($amount / $this->rates[$to][$from], 6);
+			} elseif ($from === $to) {
+				return $amount;
+			} else {
+				return 'Currency conversion rate not found';
+			}
+			return $converted;
+		}
+	}
--- tests/Unit/CurrencyConversionTest.php
+++ tests/Unit/CurrencyConversionTest.php
@@ -4,80 +4,67 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-
 use App\Support\Calculations\CurrencyConvertion;
 use PHPUnit\Framework\TestCase;
 
-	class CurrencyConversionTest extends TestCase
-	{
-		/**
-		 * @test
-		 * @dataProvider data_provider_curency_convertion_is_working
-		 */
-		public function curency_convertion_is_working(float $amount, string $from, string $to, $expected)
-		{
-			$exchange = new CurrencyConvertion();
-			$returned = $exchange->convert($amount, $from, $to);
-			$this->assertEquals($expected, $returned);
-		}
+class CurrencyConversionTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider data_provider_curency_convertion_is_working
+     */
+    public function curency_convertion_is_working(float $amount, string $from, string $to, $expected)
+    {
+        $exchange = new CurrencyConvertion();
+        $returned = $exchange->convert($amount, $from, $to);
+        $this->assertEquals($expected, $returned);
+    }
 
-		public function data_provider_curency_convertion_is_working(): array
-		{
-			return [
-				[
-					1,
-					'EUR',
-					'USD',
-					1.1497,
-				],
-				[
-					1,
-					'EUR',
-					'JPY',
-					129.53,
-				],
-				[
-					1,
-					'EUR',
-					'XXX',
-					'cant convert to that currency',
-				],
-				[
-					1,
-					'USD',
-					'EUR',
-					0.869792,
-				],
-				[
-					1,
-					'JPY',
-					'EUR',
-					0.007720,
-				],
-				[
-					1,
-					'JPY',
-					'DCS',
-					'cant convert to that currency',
-				],
-				[
-					1,
-					'EUR',
-					'EUR',
-					1,
-				],
-				[
-					1,
-					'USD',
-					'JPY',
-					112.664158,
-				],
-				[
-					1,
-					'JPY',
-					'USD',
-					0.008876,
-				],
-			];
-		}
-	}
+    public function data_provider_curency_convertion_is_working(): array
+    {
+        return [
+                [
+                    1,
+                    'EUR',
+                    'USD',
+                    1.1497,
+                ],
+                [
+                    1,
+                    'EUR',
+                    'JPY',
+                    129.53,
+                ],
+                [
+                    1,
+                    'EUR',
+                    'XXX',
+                    'Currency conversion rate not found',
+                ],
+                [
+                    1,
+                    'USD',
+                    'EUR',
+                    0.869792,
+                ],
+                [
+                    1,
+                    'JPY',
+                    'EUR',
+                    0.007720,
+                ],
+                [
+                    1,
+                    'JPY',
+                    'DCS',
+                    'Currency conversion rate not found',
+                ],
+                [
+                    1,
+                    'EUR',
+                    'EUR',
+                    1,
+                ],
+            ];
+    }
+}
