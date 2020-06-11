b1ee1e9bc330e011f01dc506a98bf27bbba6cf1b
savickasp <savickas.p@gmail.com>
2020-06-11 16:38:15 +0300

    other

--- tests/Unit/FeeControllerUnitTest.php
+++ tests/Unit/FeeControllerUnitTest.php
@@ -4,38 +4,37 @@ declare(strict_types=1);
 
 namespace Tests\Unit;
 
-
 use App\FeesController;
 use PHPUnit\Framework\TestCase;
 
-	class FeeControllerUnitTest extends TestCase
-	{
-		/**
-		 * @test
-		 * @dataProvider data_provider
-		 */
-		public function has_access_to_different_classes(string $expected, string $method)
-		{
-			$app = new FeesController();
-			$string = 'getUsersInstance';
-			$this->assertInstanceOf($expected, $app->$method());
-		}
+class FeeControllerUnitTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider data_provider
+     */
+    public function has_access_to_different_classes(string $expected, string $method)
+    {
+        $app = new FeesController();
+        $string = 'getUsersInstance';
+        $this->assertInstanceOf($expected, $app->$method());
+    }
 
-		public function data_provider()
-		{
-			return [
-				[
-					'App\Model\Users',
-					'getUsersInstance'
-				],
-				[
-					'App\Support\Files\CsvReader',
-					'getFilesInstance'
-				],
-				[
-					'App\Support\Calculations\Fees',
-					'getFeesInstance'
-				],
-			];
-		}
-	}
+    public function data_provider()
+    {
+        return [
+                [
+                    'App\Model\Users',
+                    'getUsersInstance',
+                ],
+                [
+                    'App\Support\Files\CsvReader',
+                    'getFilesInstance',
+                ],
+                [
+                    'App\Support\Calculations\Fees',
+                    'getFeesInstance',
+                ],
+            ];
+    }
+}
